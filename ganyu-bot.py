6dce7ab350b0294772f4f62b36ca3d7847c2fa69
Cody_Bagwandeen <codybagwandeen1@gmail.com>
2021-01-15 15:50:58 -0500

    created test hello command

--- ganyu-bot.py
+++ ganyu-bot.py
@@ -7,4 +7,12 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Cocogoat has arrived')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(".hello"):
+        await message.channel.send("Hello!")
+
 client.run('Nzk5Njk2NDE0NTEwMTUzNzg4.YAHVUg.dLo2Kp814dIgQEHC2ccFYY-aa44')

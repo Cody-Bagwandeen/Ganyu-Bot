bad69e44f4155bc34e4f470429e1cddfc8dca420
Cody_Bagwandeen <codybagwandeen1@gmail.com>
2021-01-16 15:47:44 -0500

    added a custom discord status to the on_ready command

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,22 +7,26 @@ class Admin(commands.Cog):
         self.client = client
 
     @commands.command()
+    #clears messages
     async def clear(self,ctx,amount = 5):
         if amount == 0:
             return
         await ctx.channel.purge(limit = (amount +1))
 
     @commands.command()
+    #kicks users from server
     async def kick(self,ctx, member : discord.Member,*, reason = None):
         await member.kick(reason=reason)
         await ctx.send(f'Kicked {member.mention}')
 
     @commands.command()
+    #bans users from server
     async def ban(self,ctx, member : discord.Member,*, reason = None):
         await member.ban(reason=reason)
         await ctx.send(f'Banned {member.mention}')
 
     @commands.command()
+    #unbans banned users from server
     async def unban(self,ctx,*,member):
         banned_users = await ctx.guild.bans() #banned users of the server
         member_name, member_discriminator = member.split('#')
@@ -36,10 +40,12 @@ class Admin(commands.Cog):
                 return
 
     @commands.Cog.listener()
+    #prints message when user joins server
     async def on_member_join(self,member):
         print(f'{member} has joined the server')
 
     @commands.Cog.listener()
+    #pints message when user leaves server
     async def on_member_remove(self,member):
         print(f'{member} has left the server.')
 
--- ganyu-bot.py
+++ ganyu-bot.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-from random import choice
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = '.' , intents = intents)
 
@@ -15,10 +14,11 @@ async def hello(ctx):
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
+#on_ready prints a message when the bot is ready, changes status when bot is on
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity = discord.Game('Working Overtime'))
     print('Cocogoat has arrived.')
-
 
 #load command
 @client.command()
@@ -33,6 +33,6 @@ async def unload(ctx,extension):
 #loop to load all extensions when the bot starts up
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+        client.load_extension(f'cogs.{filename[:-3]}') # removes the .py when trying to load the file since its checked for
 
 client.run('Nzk5Njk2NDE0NTEwMTUzNzg4.YAHVUg.dLo2Kp814dIgQEHC2ccFYY-aa44')
